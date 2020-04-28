92c6b680dfef66b6b5a2817552ff7ec24b8579a8
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2020-04-28 14:54:20 +0530

    Expressions accepts predicate

--- src/main/java/com/example/rules/dsl/Expression.java
+++ src/main/java/com/example/rules/dsl/Expression.java
@@ -1,4 +1,19 @@
 package com.example.rules.dsl;
 
-public class Expression {
+import java.util.function.Predicate;
+
+public class Expression<T> {
+    private Predicate<T> predicate;
+
+    public Expression(Predicate<T> predicate) {
+        this.predicate = predicate;
+    }
+
+    public Expression() {
+        //default no-arg constructor
+    }
+
+    public boolean execute(T t) {
+        return predicate != null && predicate.test(t);
+    }
 }
--- src/test/java/com/example/rules/dsl/RulesTest.java
+++ src/test/java/com/example/rules/dsl/RulesTest.java
@@ -2,12 +2,21 @@ package com.example.rules.dsl;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class RulesTest {
+
     @Test
     public void shouldBeAbleToCreateRule() {
         Expression ex = new Expression();
         assertNotNull(ex);
+        assertFalse(ex.execute(new Object()));
+    }
+
+    @Test
+    public void shouldBeAbleToEvaluateSimpleExpression() {
+        Expression<String> ex = new Expression<>(s -> s != null);
+        boolean result = ex.execute(new String());
+        assertTrue(result);
     }
 }
